b1d60b6b881ecb91a04c99ab44fd741d500d1cc7
Arjun <arjundutta91@gmail.com>
2016-02-29 08:34:25 -0800

    edit seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,43 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 user = User.new
-
 user.username = "adutta"
 user.password = "password"
-
 user.save!
+
+user = User.new
+user.username = "guest"
+user.password = "password"
+user.save!
+
+song = Song.new
+song.title = "Oviedo"
+song.artist_name = "Blind Pilot"
+song.user_id = 1
+song.audio_url = "http://res.cloudinary.com/dzyfczxnr/video/upload/v1456435988/ofl1g5t7npnjfdsg4uif.m4a"
+song.album_id = 1
+song.save!
+
+song = Song.new
+song.title = "Oh Madness"
+song.artist_name = "Nikhil D."
+song.user_id = 2
+song.audio_url = "http://res.cloudinary.com/dzyfczxnr/video/upload/v1456437716/mokiiyp9pcgajnbuok4d.mp3"
+song.album_id = 2
+song.save!
+
+song = Song.new
+song.title = "1922 Blues"
+song.artist_name = "Charlie Parr"
+song.user_id = 1
+song.audio_url = "http://res.cloudinary.com/dzyfczxnr/video/upload/v1456443974/k4bbmctbv81ok6o4yqvp.m4a"
+song.album_id = 3
+song.save!
+
+song = Song.new
+song.title = "Short Clip"
+song.artist_name = "Lucius Fox"
+song.user_id = 2
+song.audio_url = "http://res.cloudinary.com/dzyfczxnr/video/upload/v1456511390/dytad9rkdn5zuzymukd0.mp3"
+song.album_id = 1
+song.save!
