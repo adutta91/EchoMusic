8710e3294a46b2fe9c52d30777106c874652d289
Arjun <arjundutta91@gmail.com>
2016-03-01 14:03:49 -0800

    remake seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ song = Song.new
 song.title = "Oviedo"
 song.artist_name = "Blind Pilot"
 song.user_id = 1
-song.audio_url = "http://res.cloudinary.com/dzyfczxnr/video/upload/v1456435988/ofl1g5t7npnjfdsg4uif.m4a"
+song.audio_url = "http://res.cloudinary.com/dzyfczxnr/video/upload/v1456869546/Oviedo.m4a"
 song.album_id = 1
 song.save!
 
@@ -28,7 +28,7 @@ song = Song.new
 song.title = "Oh Madness"
 song.artist_name = "Nikhil D."
 song.user_id = 2
-song.audio_url = "http://res.cloudinary.com/dzyfczxnr/video/upload/v1456437716/mokiiyp9pcgajnbuok4d.mp3"
+song.audio_url = "http://res.cloudinary.com/dzyfczxnr/video/upload/v1456869465/Oh%20Madness.mp3"
 song.album_id = 2
 song.save!
 
@@ -36,7 +36,7 @@ song = Song.new
 song.title = "1922 Blues"
 song.artist_name = "Charlie Parr"
 song.user_id = 1
-song.audio_url = "http://res.cloudinary.com/dzyfczxnr/video/upload/v1456443974/k4bbmctbv81ok6o4yqvp.m4a"
+song.audio_url = "http://res.cloudinary.com/dzyfczxnr/video/upload/v1456869646/1922%20Blues.m4a"
 song.album_id = 3
 song.save!
 
@@ -44,6 +44,6 @@ song = Song.new
 song.title = "Short Clip"
 song.artist_name = "Lucius Fox"
 song.user_id = 2
-song.audio_url = "http://res.cloudinary.com/dzyfczxnr/video/upload/v1456511390/dytad9rkdn5zuzymukd0.mp3"
+song.audio_url = "http://res.cloudinary.com/dzyfczxnr/video/upload/v1456869740/Short%20Clip.mp3"
 song.album_id = 1
 song.save!
